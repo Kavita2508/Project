61f4925c88e09be45c2d39f00122777a88006944
Kavita2508 <101554350+Kavita2508@users.noreply.github.com>
2022-04-08 12:37:57 +0530

    Add files via upload

--- NewTest.java
+++ NewTest.java
@@ -1,10 +1,10 @@
-package rediffTest;
+package test;
 
 import org.testng.annotations.Test;
-
-import io.netty.util.AsciiString;
-
 import org.testng.annotations.BeforeTest;
+
+import java.io.IOException;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -12,57 +12,45 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 
 public class NewTest {
- public static WebDriver driver;
+	public static WebDriver driver;
 	 
-@Test
-  public void rediffTest() throws InterruptedException {
-	Thread.sleep(1000);
-  // String [][] array = new String[5][5]; 
- //   String portFolio = array[1][0];
-	 driver.findElement(By.xpath("//*[@id=\'createPortfolio\']/img")).click();
-		driver.findElement(By.xpath("//*[@id=\'create\']")).clear();
-		driver.findElement(By.xpath("//*[@id=\'create\']")).sendKeys("Large Cap Stock");
-		driver.findElement(By.xpath("//*[@id=\'createPortfolioButton\']")).click();
-
- 
-		Thread.sleep(1000);
-     	driver.findElement(By.xpath("//*[@id=\'createPortfolio\']/img")).click();
-		driver.findElement(By.xpath("//*[@id=\'create\']")).clear();
-		driver.findElement(By.xpath("//*[@id=\'create\']")).sendKeys("Mid Cap Stock");
-		driver.findElement(By.xpath("//*[@id=\'createPortfolioButton\']")).click();
+	@Test
+	  public void rediffTest() throws InterruptedException, IOException {
+		Thread.sleep(5000);
+	 
+		String []portfolioName = excelData.readXL1();
 		
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("//*[@id=\'createPortfolio\']/img")).click();
-		driver.findElement(By.xpath("//*[@id=\'create\']")).clear();
-		driver.findElement(By.xpath("//*[@id=\'create\']")).sendKeys("Small Cap Stock");
-		driver.findElement(By.xpath("//*[@id=\'createPortfolioButton\']")).click();
+		for(int counter =1; counter<portfolioName.length;counter++)
+		{
+		    driver.findElement(By.xpath("//*[@id=\'createPortfolio\']/img")).click();
+			driver.findElement(By.xpath("//*[@id=\'create\']")).clear();
+			driver.findElement(By.xpath("//*[@id=\'create\']")).sendKeys(portfolioName[counter]);
+			driver.findElement(By.xpath("//*[@id=\'createPortfolioButton\']")).click();
+			Thread.sleep(5000);
+		}
+	 
+			
+	  }
+	  @BeforeTest
+	  public void beforeTest() {
+		  System.setProperty("webdriver.chrome.driver","C:\\Drivers\\chromedriver_win32\\chromedriver.exe");
+	       WebDriver driver=new ChromeDriver();	
+driver.get("https://portfolio.rediff.com/portfolio-login");
 		
-		Thread.sleep(1000);
-		driver.findElement(By.xpath("//*[@id=\'createPortfolio\']/img")).click();
-		driver.findElement(By.xpath("//*[@id=\'create\']")).clear();
-		driver.findElement(By.xpath("//*[@id=\'create\']")).sendKeys("Mutual Fund");
-
- 		driver.findElement(By.xpath("//*[@id=\'createPortfolioButton\']")).click();
-
-		
-  }
-  @BeforeTest
-  public void beforeTest() {
-	  System.setProperty("webdriver.chrome.driver","D:\\drivers\\chromedriver_win32\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.get("https://portfolio.rediff.com/portfolio-login");
-		driver.manage().window().maximize();
 		WebElement uname = driver.findElement(By.id("useremail"));
 		uname.sendKeys("kavitakhajure606@gmail.com");
+		
 		driver.findElement(By.id("userpass")).sendKeys("Kavita@2508");
-   	driver.findElement(By.id("loginsubmit")).click();
-   	
-   	
- }
+		
+		driver.findElement(By.id("loginsubmit")).click();
+	   	
+	 }
 
-  @AfterTest
-  public void afterTest() {
-	  
-  }
+	  @AfterTest
+	  public void afterTest() throws InterruptedException {
+		  Thread.sleep(5000);
+		  driver.quit();
+		  
+	  }
 
-}
+	}
